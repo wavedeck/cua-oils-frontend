ba0fda41b691b2d2d8a7c192fbdb9ada06355ef9
wavedeck <david@invadox.at>
2024-01-31 11:30:47 +0100

    delete tanstack-query.ts

--- plugins/tanstack-query.ts
+++
@@ -1,6 +0,0 @@
-import {VueQueryPlugin} from "@tanstack/vue-query";
-import {defineNuxtPlugin} from "#app/nuxt";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(VueQueryPlugin)
-});
