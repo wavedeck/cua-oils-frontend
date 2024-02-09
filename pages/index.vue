3597d24b08a660baee35d19f4e0346d87c2c24dc
wavedeck <david@invadox.at>
2024-02-09 20:19:49 +0100

    replace intro text

--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,11 @@
   <PageHero title="BMS OILS ACADEMY" subtitle="Kundenbereich" />
   <section id="section-6-485" class="ct-section">
     <div class="container mx-auto">
-      <div
-        id="text_block-7-485"
-        class="ct-text-block"
-        style="text-align: center"
-      >
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
-      </div>
-      <div
-        id="text_block-8-485"
-        class="ct-text-block"
-        style="text-align: center; margin-top: 2rem"
-      >
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+      <div id="text_block-7-485" class="ct-text-block" style="text-align: center">
+        <p class="p1">Entdecke die vielseitige Welt der ätherischen Öle, die dein Wohlbefinden auf natürliche Weise unterstützen. Hier findest du eine umfangreiche Auswahl an <strong>Rezepten und Anwendungstipps</strong>, die von der <em>Stärkung deines Immunsystems</em> über <em>einzelne Öle</em> und <em>Ölmischungen</em>, <em>Nahrungsergänzungsmittel</em> und andere <em>wertvolle Wellnessprodukte</em> bis hin zu <em>Beauty-Tipps</em>, <em>DIY-Rezepten</em> und Tipps für einen <em>umweltfreundlichen Haushalt</em> reichen.</p>
+        <p class="p1">Diese Plattform dient als <em>Inspirationsquelle</em> und<em> Informationsbasis</em>, um das Potenzial ätherischer Öle und anderer Produkte von dōTERRA zu entdecken und zu nutzen. Jedes Öl, jede Ölmischung und jedes Rezept bietet dir die Möglichkeit, deinen Alltag gesünder und harmonischer zu gestalten und dein <em>körperliches und seelisches Wohlbefinden</em> zu steigern.</p>
+        <p class="p1">Egal, ob du gerade erst beginnst, die Welt der ätherischen Öle zu erkunden, oder deine Kenntnisse vertiefen möchtest, diese Plattform bietet dir wertvolle Informationen über grundlegende Anwendungen, Wirkungsweisen bis hin zu speziellen Tipps, um bewusste Entscheidungen für deine Gesundheit und dein Wohlbefinden zu treffen.</p>
+        <p class="p1">Beginne oder vertiefe deine Reise der Selbstentfaltung und ganzheitlichem Wohlbefinden mit dōTERRA, wo die Kraft der Natur die Gelegenheit bietet, die Balance zwischen Körper, Geist und Seele zu stärken.</p>
       </div>
     </div>
   </section>
