540a7bbf1b27300cd62cb081cf911e3f8da2414e
wavedeck <david@invadox.at>
2024-02-19 02:34:10 +0100

    replace intro text

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,25 @@
   <PageHero title="BMS OILS ACADEMY" subtitle="Kundenbereich" />
   <section id="section-6-485" class="ct-section">
     <div class="container mx-auto">
-      <div id="text_block-7-485" class="ct-text-block" style="text-align: center">
-        <p class="p1">Entdecke die vielseitige Welt der ätherischen Öle, die dein Wohlbefinden auf natürliche Weise unterstützen. Hier findest du eine umfangreiche Auswahl an <strong>Rezepten und Anwendungstipps</strong>, die von der <em>Stärkung deines Immunsystems</em> über <em>einzelne Öle</em> und <em>Ölmischungen</em>, <em>Nahrungsergänzungsmittel</em> und andere <em>wertvolle Wellnessprodukte</em> bis hin zu <em>Beauty-Tipps</em>, <em>DIY-Rezepten</em> und Tipps für einen <em>umweltfreundlichen Haushalt</em> reichen.</p>
-        <p class="p1">Diese Plattform dient als <em>Inspirationsquelle</em> und<em> Informationsbasis</em>, um das Potenzial ätherischer Öle und anderer Produkte von dōTERRA zu entdecken und zu nutzen. Jedes Öl, jede Ölmischung und jedes Rezept bietet dir die Möglichkeit, deinen Alltag gesünder und harmonischer zu gestalten und dein <em>körperliches und seelisches Wohlbefinden</em> zu steigern.</p>
-        <p class="p1">Egal, ob du gerade erst beginnst, die Welt der ätherischen Öle zu erkunden, oder deine Kenntnisse vertiefen möchtest, diese Plattform bietet dir wertvolle Informationen über grundlegende Anwendungen, Wirkungsweisen bis hin zu speziellen Tipps, um bewusste Entscheidungen für deine Gesundheit und dein Wohlbefinden zu treffen.</p>
-        <p class="p1">Beginne oder vertiefe deine Reise der Selbstentfaltung und ganzheitlichem Wohlbefinden mit dōTERRA, wo die Kraft der Natur die Gelegenheit bietet, die Balance zwischen Körper, Geist und Seele zu stärken.</p>
+      <div
+        id="text_block-7-485"
+        class="ct-text-block"
+        style="text-align: center"
+      >
+        <p>
+          Entdecke mit uns die vielseitige Welt der ätherischen Öle, um dein
+          Wohlbefinden auf natürliche Weise zu unterstützen. In der BMS Oils
+          Academy findest du eine kraftvolle Auswahl an
+          <strong>DIY Rezepten und Anwendungstipps</strong>. Klicke dafür gerne
+          auf nachfolgende Kacheln, um zu den jeweiligen
+          <strong>Rezeptkategorien</strong> zu gelangen.
+        </p>
+        <p>
+          Nutze auch sehr gerne das <strong>BMS Oils Lexikon</strong>. Dort
+          kannst du jegliches ätherisches Öl oder ätherische Öle Mischung von
+          doTerra nachschlagen, und erfährst ihre Wirkung auf körperlicher,
+          emotionaler und feinstofflicher Ebene.
+        </p>
       </div>
     </div>
   </section>
